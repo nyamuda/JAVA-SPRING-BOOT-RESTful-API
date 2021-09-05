937346b1d3655f405c3a465542279d3b54e269dc
Pierce Nyamuda <ptnrlab@gmail.com>
2021-09-05 18:25:32 +0200

    JWT maven dependency added

--- src/main/java/com/physicsdefinitions/science/Security/MyConfiguration.java
+++ src/main/java/com/physicsdefinitions/science/Security/MyConfiguration.java
@@ -3,8 +3,12 @@ package com.physicsdefinitions.science.Security;
 import com.physicsdefinitions.science.Services.UserDetailsImplementation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class MyConfiguration extends WebSecurityConfigurerAdapter {
@@ -22,5 +26,20 @@ public class MyConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // telling the authenticationManger how the user user should be authenticated
         auth.userDetailsService(userDetails).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.csrf().disable();
+        http.formLogin().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
