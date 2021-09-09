ef28f24e7267bf8b233dcfdd7823fb4d2bf2d8f0
Pierce Nyamuda <ptnrlab@gmail.com>
2021-09-09 19:54:21 +0200

    user/admin can register

--- src/main/java/com/physicsdefinitions/science/Security/MyConfiguration.java
+++ src/main/java/com/physicsdefinitions/science/Security/MyConfiguration.java
@@ -37,12 +37,12 @@ public class MyConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/login").permitAll().antMatchers("/user/save")
-                .hasAnyAuthority("USER", "ADMIN").antMatchers("/curriculum**").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers("/subject**").hasAnyAuthority("USER", "ADMIN").antMatchers("/topic**")
-                .hasAnyAuthority("USER", "ADMIN").antMatchers("/term**").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers("/topic**").hasAnyAuthority("USER", "ADMIN").antMatchers("/users").permitAll()
-                .antMatchers("/role**").hasAuthority("ADMIN").antMatchers("/user/add_role").hasAuthority("ADMIN");
+        http.authorizeRequests().antMatchers("/login").permitAll().antMatchers("/user/save").permitAll()
+                .antMatchers("/curriculum**").hasAnyAuthority("USER", "ADMIN").antMatchers("/subject**")
+                .hasAnyAuthority("USER", "ADMIN").antMatchers("/topic**").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers("/term**").hasAnyAuthority("USER", "ADMIN").antMatchers("/topic**")
+                .hasAnyAuthority("USER", "ADMIN").antMatchers("/users").permitAll().antMatchers("/role**")
+                .hasAuthority("ADMIN").antMatchers("/user/add_role").hasAuthority("ADMIN");
         http.csrf().disable();
         http.formLogin().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
